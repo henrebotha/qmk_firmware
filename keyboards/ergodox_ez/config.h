3e15278d8d1fdb348d7e08c11411a5c193255392
Henré Botha <henrebotha@gmail.com>
2017-12-16 19:06:08 +0200

    Remove some unused stuff

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -48,10 +48,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM    200
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+/* /1* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap *1/ */
+/* #define LOCKING_SUPPORT_ENABLE */
+/* /1* Locking resynchronize hack *1/ */
+/* #define LOCKING_RESYNC_ENABLE */
 
 /* key combination for command */
 #define IS_COMMAND() ( \
