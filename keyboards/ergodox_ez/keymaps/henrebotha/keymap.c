f90db4c257720c1fce4e4695a928cc59805ad58d
Henré Botha <henrebotha@gmail.com>
2017-12-22 12:15:32 +0200

    Tap toggle all function layers

--- keyboards/ergodox_ez/keymaps/henrebotha/keymap.c
+++ keyboards/ergodox_ez/keymaps/henrebotha/keymap.c
@@ -1,4 +1,4 @@
-// v1.2.6
+// v1.2.7
 
 #include "ergodox_ez.h"
 #include "debug.h"
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,           KC_Q,     KC_W,    KC_E,     KC_R,      KC_T,             LCTL(KC_B),
       CTL_T(KC_ESCAPE), KC_A,     KC_S,    KC_D,     KC_F,      KC_G,
       KC_LGUI,          KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,             KC_MINUS,
-      OSL(2),           KC_TRNS,  KC_TRNS, OSL(3),   OSL(2),
+      TT(2),            KC_TRNS,  KC_TRNS, TT(3),    TT(2),
 
       KC_MUTE,          KC_VOLU,
       KC_VOLD,
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,          KC_Y,     KC_U,    KC_I,     KC_O,      KC_P,             KC_BSLASH,
       KC_H,             KC_J,     KC_K,    KC_L,     KC_SCOLON, KC_QUOTE,
       KC_EQUAL,         KC_N,     KC_M,    KC_COMMA, KC_DOT,    KC_SLASH,         KC_RGUI,
-      OSL(2),           OSL(3),   KC_TRNS, KC_TRNS,  OSL(2),
+      TT(2),            TT(3),   KC_TRNS, KC_TRNS,   TT(2),
 
       KC_MPRV,          KC_MNXT,
       KC_MPLY,
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,   KC_PGUP,   KC_HOME,     KC_UP,    KC_END,      KC_TRNS, KC_TRNS,
       KC_TRNS,   KC_PGDOWN, KC_LEFT,     KC_DOWN,  KC_RIGHT,    KC_TRNS,
       KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_LPRN,  KC_LBRACKET, KC_LCBR, KC_TRNS,
-      KC_NO,     KC_TRNS,   KC_TRNS,     KC_TRNS,  KC_NO,
+      KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS,  KC_TRNS,
 
       KC_TRNS,   KC_TRNS,
       KC_TRNS,
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS,  KC_F11,      KC_F12,  KC_INSERT,
       KC_LEFT,   KC_DOWN,   KC_UP,       KC_RIGHT, KC_QUOTE,    KC_TRNS,
       KC_TRNS,   KC_RCBR,   KC_RBRACKET, KC_RPRN,  KC_TRNS,     KC_TRNS, KC_TRNS,
-      KC_NO,     KC_TRNS,   KC_TRNS,     KC_TRNS,  KC_NO,
+      KC_TRNS,   KC_TRNS,   KC_TRNS,     KC_TRNS,  KC_TRNS,
 
       KC_TRNS,   KC_TRNS,
       KC_TRNS,
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    TMUX_ML,
       KC_TRNS, TMUX_A,  TMUX_S,  TMUX_D,  KC_TRNS, KC_TRNS,
       KC_TRNS, TMUX_Z,  KC_TRNS, TMUX_C,  KC_TRNS, LCTL(KC_B), TMUX_SH,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
       KC_TRNS, KC_TRNS,
       KC_TRNS,
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TMUX_MR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    TMUX_SV,
                TMUX_H,  TMUX_J,  TMUX_K,  TMUX_L,  TMUX_CL,    KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,
-                        KC_TRNS, KC_NO,   KC_TRNS, KC_TRNS,    KC_TRNS,
+                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,
 
       KC_TRNS, KC_TRNS,
       KC_TRNS,
