9a399cfef8cd33afd82628af2301f5a637f1aa38
Henré Botha <henrebotha@gmail.com>
2017-12-28 15:18:29 +0200

    Use proper oneshot toggling

--- keyboards/ergodox_ez/keymaps/henrebotha/keymap.c
+++ keyboards/ergodox_ez/keymaps/henrebotha/keymap.c
@@ -1,4 +1,4 @@
-// v1.2.7
+// v1.3
 
 #include "ergodox_ez.h"
 #include "debug.h"
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,           KC_Q,     KC_W,    KC_E,     KC_R,      KC_T,             LCTL(KC_B),
       CTL_T(KC_ESCAPE), KC_A,     KC_S,    KC_D,     KC_F,      KC_G,
       KC_LGUI,          KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,             KC_MINUS,
-      TT(2),            KC_TRNS,  KC_TRNS, TT(3),    TT(2),
+      OSL(2),           KC_TRNS,  KC_TRNS, OSL(3),   OSL(2),
 
       KC_MUTE,          KC_VOLU,
       KC_VOLD,
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,          KC_Y,     KC_U,    KC_I,     KC_O,      KC_P,             KC_BSLASH,
       KC_H,             KC_J,     KC_K,    KC_L,     KC_SCOLON, KC_QUOTE,
       KC_EQUAL,         KC_N,     KC_M,    KC_COMMA, KC_DOT,    KC_SLASH,         KC_RGUI,
-      TT(2),            TT(3),   KC_TRNS, KC_TRNS,   TT(2),
+      OSL(2),           OSL(3),   KC_TRNS, KC_TRNS,  OSL(2),
 
       KC_MPRV,          KC_MNXT,
       KC_MPLY,
